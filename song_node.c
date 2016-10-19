3b5bdb239339958b776fda6ab8abf2fc0d9838a3
71619997a <gmarks@stuy.edu>
2016-10-19 16:32:13 -0400

    Add tests & typedfef things

--- song_node.c
+++ song_node.c
@@ -2,77 +2,79 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct Song {
-    char *name;
-    char *artist;
-};
+typedef struct Song {
+    char name[256];
+    char artist[256];
+} Song;
 
-struct SongNode {
-    struct Song *song;
+typedef struct SongNode {
+    Song *song;
     struct SongNode *next;
-};
+} SongNode;
 
-struct SongNode* insert_front(struct SongNode* start, struct Song* song) {
-    struct SongNode *newSongNode = (struct SongNode*)malloc(16);
+SongNode* insert_front(SongNode* start, Song* song) {
+    SongNode *newSongNode = (SongNode*)malloc(sizeof(SongNode));
     newSongNode->song = song;
     newSongNode->next = start;
     return newSongNode;
 }
 
-struct SongNode* insert_ordered(struct SongNode* start, struct Song* song) {
-  if(start == NULL || strcmp(song->name, start->song->name) <= 0) {
-    return insert_front(start, song);
-  }
-  struct SongNode *last = start;
-  struct SongNode *iter = start->next;
-  while(iter != NULL) {
-    if(strcmp(song->name, iter->song->name) <= 0) {
-      struct SongNode *new = insert_front(iter, song);
-      last->next = new;
+SongNode* insert_ordered(SongNode* start, Song* song) {
+    if(start == NULL || strcmp(song->name, start->song->name) <= 0) {
+        return insert_front(start, song);
     }
-  }
-  return start;
+    SongNode *last = start;
+    SongNode *iter = start->next;
+    while(iter != NULL) {
+        if(strcmp(song->name, iter->song->name) <= 0) {
+            SongNode *new = insert_front(iter, song);
+            last->next = new;
+            break;
+        }
+        iter = start->next;
+    }
+    return start;
 }
 
-struct SongNode* free_list(struct SongNode* start) {
+SongNode* free_list(SongNode* start) {
     if(start == NULL) {
         return NULL;
     }
-    free_list(start->next);  // make sure all memory past is gone
-    start->next = NULL;  // make sure we aren't pointing to freed memory
-    free(start->song);
+    start->next = free_list(start->next); // free and point
+    free(start->song); // same as last
     start->song = NULL;
-    free(start);  // now we can free this memory
-    return NULL;
+    free(start); // now we can free this memory
+    return NULL; // don't use the pointer to freed mem
 }
 
-void print_list(struct SongNode* start) {
+void print_list(SongNode* start) {
     if(start == NULL || start->song == NULL) {
         printf("NULL\n");
         return;
     }
-    printf("%s - %s -> ", start->song->artist, start->song->name);
+    printf("\"%s\" by %s -> ", start->song->name, start->song->artist);
     print_list(start->next);
 }
 
-struct Song *newSong(char *artist, char *name) {
-  struct Song *new = (struct Song*)malloc(sizeof(struct Song));
-  new->name = name;
-  new->artist = artist;
-  return new;
+Song *new_song(char *artist, char *name) {
+    Song *new = (Song*)malloc(sizeof(Song));
+    strcpy(new->name, name);
+    strcpy(new->artist, artist);
+    return new;
 }
 
 int main() {
-  struct Song *s1 = newSong("a guy", "some song");
-  struct SongNode *sn1 = insert_ordered(NULL, s1);
-  sn1 = insert_ordered(sn1, s1); //two copies
-  print_list(sn1);
-  struct Song *s2 = newSong("other guy", "another song");
-  sn1 = insert_ordered(sn1, s2);
-  printf("inserted 2");
-  struct Song *s3 = newSong("other guy", "his other song");
-  sn1 = insert_ordered(sn1, s3);
-  print_list(sn1);
-  sn1 = free_list(sn1);
-  print_list(sn1);
+    setbuf(stdout, NULL);  // for debug
+    Song *s1 = new_song("a guy", "some song");
+    SongNode *sn1 = insert_ordered(NULL, s1);
+    sn1 = insert_ordered(sn1, s1); //two copies
+    print_list(sn1);
+    Song *s2 = new_song("other guy", "another song");
+    sn1 = insert_ordered(sn1, s2);
+    print_list(sn1);
+    Song *s3 = new_song("other guy", "His other song");
+    sn1 = insert_ordered(sn1, s3);
+    print_list(sn1);
+    sn1 = free_list(sn1);
+    print_list(sn1);
 }
