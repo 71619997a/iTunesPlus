818a297ab890663fb987dbdfec6f8e1afe030c24
71619997a <gmarks@stuy.edu>
2016-10-20 18:26:48 -0400

    Update Playlist.h with new functions

--- Playlist.h
+++ Playlist.h
@@ -1,5 +1,12 @@
 #ifndef PLAYLIST_H
 #define PLAYLIST_H
 typedef enum characteristic {NAME, ARTIST} characteristic;
-
+void initializeTable();
+char artist_index(char);
+void addSong(Song*);
+Song **find_songs(char, characteristic);
+void print_songs(Song **);
+void print_by_letter(char);
+void print_all();
+int main();
 #endif
